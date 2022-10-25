e5795f841da13355b1f6c90cb83122b5f27a130a
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-25 23:23:44 +0500

    Routes-Express.js

---
+++ SHOPPING_LIST/routes/api/items.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { getdb, createdb, deletedb, updatedb } = require('../../mongo_database');
+const routes = express.Router();
+
+
+
+routes.get('/', (req, res) => {
+    console.log("Get request")
+    getdb(req.body).then(result => {
+        res.send(result)
+    })
+})
+
+routes.post('/', (req, res) => {
+    console.log("POST REQUEST")
+    createdb(req.body).then(result => {
+        if (result === true) {
+            res.send("Item already exist in the Database")
+        }
+        else {
+            res.send("New Item added to the Database")
+        }
+    })
+})
+
+routes.delete('/', (req, res) => {
+    deletedb(req.body).then(result => {
+        if (result === true) {
+            res.send("Item deleted from database")
+        }
+        else {
+            res.send("Item does not exist in the database.")
+        }
+    })
+
+})
+
+routes.put('/', (req, res) => {
+    updatedb(req.body).then(result => {
+        if (result === true) {
+            res.send("Item Updated")
+        }
+        else {
+            res.send("Item does not exist in the database.")
+        }
+    })
+})
+
+module.exports = routes
--- SHOPPING_LIST/server.js
+++ SHOPPING_LIST/server.js
@@ -1,57 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors');
-const { getdb, createdb, deletedb, updatedb } = require('./mongo_database');
 
+//Will direct the request to the following path
+const items = require('./routes/api/items')
+
+//Creating an express application
 const app = express();
+
+//Body-parser is the Node. js body parsing middleware.
+// It is responsible for parsing the incoming request bodies in a middleware before you handle it. 
 app.use(bodyParser.json());
 app.use(cors());
 
+//Any request at /api/items will be reffered to the items variable
+app.use('/api/items', items)
 app.listen(3000, () => {
     console.log("Server started")
 })
-
-app.get('/', (req, res) => {
-    console.log("Get request")
-    getdb(req.body).then(result => {
-        res.send(result)
-    })
-})
-
-app.post('/', (req, res) => {
-    console.log("POST REQUEST")
-    createdb(req.body).then(result => {
-        if (result === true) {
-            res.send("Item already exist in the Database")
-        }
-        else {
-            res.send("New Item added to the Database")
-        }
-    })
-})
-
-app.delete('/', (req, res) => {
-    deletedb(req.body).then(result => {
-        if (result === true) {
-            res.send("Item deleted from database")
-        }
-        else {
-            res.send("Item does not exist in the database.")
-        }
-    })
-
-})
-
-app.put('/', (req, res) => {
-    updatedb(req.body).then(result => {
-        if (result === true) {
-            res.send("Item Updated")
-        }
-        else {
-            res.send("Item does not exist in the database.")
-        }
-    })
-})
-
-
-
