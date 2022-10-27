3f746a0420fab84f966c7cd05a59f83bee6cf5f3
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-27 20:26:13 +0500

    Reactstrap Navbar

--- SHOPPING_LIST/client/src/App.js
+++ SHOPPING_LIST/client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ItemsForm from './components/ItemsForm';
+import Example from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Example></Example>
       <ItemsForm></ItemsForm>
     </div>
   );
