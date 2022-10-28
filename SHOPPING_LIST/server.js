722a24a6dcacd7da38e3d9f67e69d0dfacc1be50
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-10-28 20:30:56 +0500

    Deploy on heroku

--- SHOPPING_LIST/server.js
+++ SHOPPING_LIST/server.js
@@ -19,7 +19,7 @@ app.use('/api/items', items)
 
 //Serve static assets if in production state
 if (process.env.NODE_ENV === 'production') {
-    //set static assets
+    //set static assets to index.html
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
