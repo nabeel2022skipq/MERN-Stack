f33a215dbd16534d9a08e9c8e98733e5da42c6e5
nabeelahmed-0 <nabeelahmedpk1@outlook.com>
2022-11-04 18:23:07 +0500

    Updated Css

--- ecommerce-talwindcss/src/components/ecommerce.jsx
+++ ecommerce-talwindcss/src/components/ecommerce.jsx
@@ -1,87 +1,152 @@
 import React from 'react';
 function ECOMMERCE() {
     return (
-        <div className='grid lg:grid-cols-4 gap-7 m-9 '>
-            {/* Card goes here */}
+        <div className='grid grid-cols-3'>
+            <div className='col-span-1 border-r-8 border-slate-300 bg-white'></div>
+            <main className='col-span-2 pt-10 px-3'>
+                <div className='flex justify-end item'>
+                    <span className='rounded-lg bg-slate-50 border-2 border-red-400 text-red-400 hover:bg-red-400 hover:text-white p-3 cursor-pointer transition ease-out duration-500'>LOGIN</span>
+                    <span className='rounded-lg bg-slate-50 border-2 border-red-400 text-red-400 hover:bg-red-400 hover:text-white p-3 ml-2 cursor-pointer'>SIGNUP</span>
+                </div>
+                <div className='text-center'>
+                    <h1 className='text-5xl sm:text-7xl text-slate-400 font-bold my-3'>PAKLAP</h1>
+                </div>
+                <div className='grid lg:grid-cols-3 gap-7 m-9'>
+                    {/* Card goes here */}
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
+                    <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white transform hover:scale-105 hover:transition hover:ease-out hover:duration-300'>
+                        <div className='flex justify-center'>
+                            <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
+                        </div>
+                        <div className='p-6'>
+                            <span className='block font-bold text-xl'>HP OMEN 15</span>
+                            <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
+                        </div>
+                        <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
+                            <span className='p-3'>Price: 225,000</span>
+                        </div>
+                    </div>
 
-            <div className='rounded shadow-md border-b border-black hover:shadow-2xl transition ease-out duration-300 relative  bg-white'>
-                <div className='flex justify-center'>
-                    <img src='https://www.hbc.com.pk/images/products/hp-omen-15-ek1097nr-ci7-11th-gen,-16gb,-512gb-ssd,-rtx-3060--4059-680436-130821105115.png' alt='o15' className='border-b border-gray-400 h-52 object-cover mt-2'></img>
-                </div>
-                <div className='p-6'>
-                    <span className='block font-bold text-xl'>HP OMEN 15</span>
-                    <span className='text-gray-500 text-sm'>HP Omen 15 Ci7 10th Gen 16GB, 512GB, RTX 3060</span>
-                </div>
-                <div className='absolute top-0 bg-slate-400 text-white rounded-br-md rounded-tl-md transform hover:scale-125 transition ease-out duration-300'>
-                    <span className='p-3'>Price: 225,000</span>
-                </div>
-            </div>
 
+
+                </div>
+
+                <footer className='border-slate-200 shadow-md rounded-md overflow-hidden'>
+                    <div className='grid grid-cols-2 lg:grid-cols-4 bg-slate-200'>
+                        <div className='p-10'>
+                            <h3 className='text-2xl font-bold mb-4'>Contact Us</h3>
+                            <ul className='text-slate-600 text-sm'>
+                                <li>The</li>
+                                <li>Quick</li>
+                                <li>Brown</li>
+                                <li>Fox</li>
+                                <li>Jumsp</li>
+                                <li>Over</li>
+                            </ul>
+                        </div>
+                        <div className='p-10'>
+                            <h3 className='text-2xl font-bold mb-4'>FAQ's</h3>
+                            <ul className='text-slate-600 text-sm'>
+                                <li>The</li>
+                                <li>Quick</li>
+                                <li>Brown</li>
+                                <li>Fox</li>
+                                <li>Jumsp</li>
+                                <li>Over</li>
+                            </ul>
+                        </div>
+                        <div className='p-10'>
+                            <h3 className='text-2xl font-bold mb-4'>Pricing</h3>
+                            <ul className='text-slate-600 text-sm'>
+                                <li>The</li>
+                                <li>Quick</li>
+                                <li>Brown</li>
+                                <li>Fox</li>
+                                <li>Jumsp</li>
+                                <li>Over</li>
+                            </ul>
+                        </div>
+                        <div className='p-10'>
+                            <h3 className='text-2xl font-bold mb-4'>About Us</h3>
+                            <ul className='text-slate-600 text-sm'>
+                                <li>The</li>
+                                <li>Quick</li>
+                                <li>Brown</li>
+                                <li>Fox</li>
+                                <li>Jumsp</li>
+                                <li>Over</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='flex justify-center bg-slate-300 py-1 uppercase font-bold font-mono'>
+                        <h1>Copyright @2022</h1>
+                    </div>
+                </footer>
+            </main>
         </div>
     );
 }
